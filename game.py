4ec02336b425c91e094c0f1280b1e8e902214e99
Gautam J <gautam.jayapal@gmail.com>
2020-09-04 17:19:35 +0530

    add checkWinner

--- game.py
+++ game.py
@@ -34,17 +34,23 @@ class TicTacToe:
             self.updateAttributes()
             self.render()
 
+            if self.winner is not None:
+                self.showGameoverScreen()
+                self.playing = False
+
     def handleEvents(self):
 
         self.mouseClicked = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.playing = False
-                self.isRunning = False
+                pygame.quit()
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 self.mouseClicked = True
+
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+                pygame.quit()
 
     def updateAttributes(self):
 
@@ -52,6 +58,7 @@ class TicTacToe:
         if self.mouseClicked:
             self.updateStates(mouseX, mouseY)
             self.checkWinner()
+            self.switchPlayerTurns()
 
     def updateStates(self, mouseX, mouseY):
         j = mouseX // (s.HEIGHT // 3)
@@ -60,11 +67,38 @@ class TicTacToe:
         if self.states[i][j] is None:
             print(f'[INFO] {time.time()} {self.currentPlayer} clicked at {i = } {j = }')
             self.states[i][j] = self.currentPlayer
-            self.switchPlayerTurns()
+
+    @staticmethod
+    def allEquals(x, y, z):
+        return x is not None and x == y and y == z
 
     def checkWinner(self):
-        # refer js file
-        pass
+
+        # horizontal
+        for i in range(3):
+            if self.allEquals(self.states[i][0], self.states[i][1], self.states[i][2]):
+                self.winner = self.states[i][0]
+
+        # vertical
+        for i in range(3):
+            if self.allEquals(self.states[0][i], self.states[1][i], self.states[2][i]):
+                self.winner = self.states[0][i]
+
+        # diagonal
+        if self.allEquals(self.states[0][0], self.states[1][1], self.states[2][2]):
+            self.winner = self.states[0][0]
+
+        if self.allEquals(self.states[2][0], self.states[1][1], self.states[0][2]):
+            self.winner = self.states[2][0]
+
+        openSpots = 0
+        for i in range(3):
+            for j in range(3):
+                if self.states[i][j] is None:
+                    openSpots += 1
+
+        if self.winner is None and openSpots == 0:
+            self.winner = 'Tie'
 
     def render(self):
 
@@ -108,3 +142,41 @@ class TicTacToe:
 
     def getMousePosition(self):
         return pygame.mouse.get_pos()
+
+    def showGameoverScreen(self):
+
+        if self.winner in self.players:
+            self.drawTextToScreen(f'{self.winner} won!', s.WIDTH // 2, s.HEIGHT // 3)
+        else:
+            self.drawTextToScreen(f'{self.winner}!', s.WIDTH // 2, s.HEIGHT // 3)
+
+        self.drawTextToScreen('Press any key to play again,\'q\' to quit ', s.WIDTH // 2, (s.HEIGHT * 2) // 3)
+
+        pygame.display.update()
+
+        pressed = False
+        while not pressed:
+            self.handleEvents()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYUP:
+                    pressed = True
+
+    def drawTextToScreen(self, text, x, y, size=18, color=(255, 0, 0)):
+        """Draws text to screen.
+        Args:
+            text (str): Text to be displayed.
+            size (int): Size of the text.
+            color (tuple): Color of the text.
+            x (int): x coordinate of the text.
+            y (int): y coordinate of the text.
+        """
+
+        font = pygame.font.Font(pygame.font.match_font('Arial'), size)
+        textSurface = font.render(text, True, color)  # antialiasing
+        textRect = textSurface.get_rect()
+
+        textRect.centerx = x
+        textRect.centery = y
+
+        self.screen.blit(textSurface, textRect)
