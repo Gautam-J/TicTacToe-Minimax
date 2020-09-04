ac336a2e0509080d33a0181d86ed2b07b01f618a
Gautam J <gautam.jayapal@gmail.com>
2020-09-04 13:13:46 +0530

    add switch player turn based on user click

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import time
 import pygame
 import settings as s
 
@@ -16,6 +17,10 @@ class TicTacToe:
 
     def startNewGame(self):
 
+        self.states = [[None for _ in range(3)] for _ in range(3)]
+        self.players = ['X', 'O']
+        self.currentPlayer = self.players[0]
+
         self.runGameLoop()
 
     def runGameLoop(self):
@@ -29,13 +34,22 @@ class TicTacToe:
 
     def handleEvents(self):
 
+        self.mouseClicked = False
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.playing = False
                 self.isRunning = False
 
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                self.mouseClicked = True
+
     def updateAttributes(self):
-        pass
+
+        mouseX, mouseY = self.getMousePosition()
+        if self.mouseClicked:
+            print(f'[INFO] {time.time()} {self.currentPlayer} clicked at {mouseX = } {mouseY  = }')
+            self.switchPlayerTurns()
 
     def render(self):
 
@@ -50,3 +64,12 @@ class TicTacToe:
         pygame.draw.line(self.screen, (0, 0, 0), (s.WIDTH * 2 // 3, 0), (s.WIDTH * 2 // 3, s.HEIGHT), 3)
         pygame.draw.line(self.screen, (0, 0, 0), (0, s.HEIGHT // 3), (s.WIDTH, s.HEIGHT // 3), 3)
         pygame.draw.line(self.screen, (0, 0, 0), (0, s.HEIGHT * 2 // 3), (s.WIDTH, s.HEIGHT * 2 // 3), 3)
+
+    def switchPlayerTurns(self):
+
+        currentPlayerIndex = self.players.index(self.currentPlayer)
+        nextPlayerIndex = (currentPlayerIndex + 1) % 2
+        self.currentPlayer = self.players[nextPlayerIndex]
+
+    def getMousePosition(self):
+        return pygame.mouse.get_pos()
